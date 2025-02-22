53cdfcb270bec81036e1677a0b054c21250bfecc
Clotho-ex <yusufcanvar90@gmail.com>
2025-02-22 16:48:35 +0300

    Added a button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import AllNotes from "./Components/AllNotesComponent/AllNotes";
+import Button from "./components/ButtonComponent/Button";
 import Logo from "./Components/LogoComponent/Logo";
 import SideBar from "./Components/SidebarComponent/SideBar";
 
 function App() {
   return (
     <>
-      <div className="px-4 pt-4 sm:px-4 md:px-8 lg:px-8 grid grid-cols-8 md:grid-cols-8 lg:grid-cols-12 gap-[24px] w-full">
-        <Logo />
-        <SideBar />
-        <AllNotes />
+      <div className="grid place-items-center mt-2">
+        <Button/>
       </div>
     </>
   );
